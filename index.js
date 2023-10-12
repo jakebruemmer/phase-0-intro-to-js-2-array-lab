7361031ceba1ce4ef0e96ad7c888e0dd42ebcaad
Jake Bruemmer <jakebruemmer@gmail.com>
2023-10-12 11:35:06 -0600

    Completed function methods.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+let destructivelyAppendCat = function (name) {
+  cats.push(name);
+}
+
+let destructivelyPrependCat = function (name) {
+  cats.unshift(name);
+}
+
+let destructivelyRemoveLastCat = function () {
+  cats.pop();
+}
+
+let destructivelyRemoveFirstCat = function () {
+  cats.shift();
+}
+
+let appendCat = function (name) {
+  return Array.prototype.concat(cats, name);
+}
+
+let prependCat = function (name) {
+  return Array.prototype.concat(name, cats);
+}
+
+let removeLastCat = function () {
+  return cats.slice(0, -1);
+}
+
+let removeFirstCat = function () {
+  return cats.slice(1);
+}
